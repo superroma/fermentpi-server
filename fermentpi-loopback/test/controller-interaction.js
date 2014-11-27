6746fb7594a4ec2791d8b4e44147bc6190dbd054
Roman Eremin <romae@devexpress.com>
2014-11-27 17:59:10 +0300

    Testing libs added

--- fermentpi-loopback/test/controller-interaction.js
+++ fermentpi-loopback/test/controller-interaction.js
@@ -1,7 +1,12 @@
-var expect = require('chai').expect;
+var expect = require('chai').expect,
+    supertest = require('supertest'),
+    api = supertest('http://0.0.0.0:3000');
 
-describe('Suite1', function () {
-    it('should pass by definition', function () {
-        expect(true);
-    })
+describe('Api check', function () {
+    it('should list controllers', function (done) {
+        api.get('/api/Controllers')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done)
+    });
 })
