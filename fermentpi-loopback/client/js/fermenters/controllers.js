793decb87a31cd7b8112c91a86cdc5fc4e0678ef
Roman Eremin <romae@devexpress.com>
2015-01-18 13:18:11 +0300

    tuning chart

--- fermentpi-loopback/client/js/fermenters/controllers.js
+++ fermentpi-loopback/client/js/fermenters/controllers.js
@@ -3,13 +3,17 @@
 angular
     .module('app')
     .controller('FermenterListCtrl', ['$scope', '$state', 'Controller',
-        function ($scope, 
+        function ($scope,
             $state, Controller) {
             $scope.controllers = [];
 
             function getFermenters() {
                 Controller
-                    .find({filter:{include: "Fermenters"}})
+                    .find({
+                        filter: {
+                            include: "Fermenters"
+                        }
+                    })
                     .$promise
                     .then(function (results) {
                         $scope.controllers = results;
@@ -19,22 +23,76 @@ angular
         }
     ])
     .controller('FermenterDetailCtrl', ['$scope', '$stateParams', 'Fermenter',
-        function($scope, $stateParams, Fermenter) {
+        function ($scope, $stateParams, Fermenter) {
+            function setChartOptions() {
+                $scope.chartOptions = {
+                    series: [{
+                            name: 'Fermenter temp',
+                            type: 'spline',
+                            valueField: 'Value',
+                            argumentField: 'When',
+                            point: {
+                                visible: false
+                            }
+                    }],
+                    bindingOptions: {
+                        dataSource: 'tempReadings'
+                    },
+                    animation: {
+                        enabled: false
+                    },
+                    argumentAxis: {
+                        argumentType: 'datetime',
+                        grid: {
+                            visible: true,
+                            opacity: 0.3
+                        }
+                    },
+                    valueAxis: {
+                        tickInterval: 1,
+                        grid: {
+                            opacity: 0.5
+                        }
+                    }
+                };
+                $scope.rangeOptions = {
+                    bindingOptions: $scope.chartOptions.bindingOptions,
+                    scale: {
+                        //minorTickInterval: {minutes: 10},
+                        //majorTickInterval: 'day',
+                        valueType: 'datetime'
+                    },
+                    chart: $scope.chartOptions,
+                    behavior: {
+                        snapToTicks: false,
+                        callSelectedRangeChanged: "onMoving",
+                        animationEnabled: false
+                    },
+                    onSelectedRangeChanged: function (e) {
+                        var zoomedChart = $("#chart").dxChart('instance');
+                        zoomedChart.zoomArgument(e.startValue, e.endValue);
+                    }
+                };
+            }
+            setChartOptions();
+            
             $scope.fermenterId = $stateParams.fermenterId;
             $scope.fermenter = {};
-            Fermenter.findById({id:$scope.fermenterId})
-                .$promise
-                .then(function(result) {
+            Fermenter.findById({
+                id: $scope.fermenterId
+            }).$promise
+                .then(function (result) {
                     $scope.fermenter = result;
-            });
+                });
             $scope.tempReadings = {};
             Fermenter.TempReadings({
-                id: $scope.fermenterId, 
-                where:{limit:100}
+                id: $scope.fermenterId,
+                where: {
+                    limit: 100
+                }
             }).$promise
-                .then(function(result) {
+                .then(function (result) {
                     $scope.tempReadings = result;
-            });
+                });
         }
     ]);
-                                         
